0cda6597dd58e388eb07e9157d12034ddd94abbc
jinpark113 <jinpark5676@gmail.com>
2025-09-12 19:34:31 +0900

    ch1-4 네비게이션 바

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar"; // 폴더 대소문자 주의!
+
+// import Footer from "./Components/Footer/Footer"; // Footer 파일이 있을 때만 사용
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Navbar />
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
 }
-
-export default App
+export default App;
