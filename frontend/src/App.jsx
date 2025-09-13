88e47eb34f3af383115b20649585f4e2bb3e8e00
jinpark113 <jinpark5676@gmail.com>
2025-09-13 12:32:20 +0900

    ch2-1 MainPage-hero 제작

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,62 @@
 import "./App.css";
-import { BrowserRouter } from "react-router-dom";
-import Footer from "./components/Footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
 
-function App() {
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar"; //components에서 c는 소문자
+
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+import MainPage from "./pages/MainPage/MainPage";
+import About from "./pages/About/About";
+import Leadership from "./pages/Leadership/Leadership";
+import Board from "./pages/Board/Board";
+import Services from "./pages/Services/Services";
+import Contact from "./pages/Contact/Contact";    //pages에서 p는 소문자 
+
+function Layout() {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
+      <Outlet />
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <MainPage />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/leadership",
+        element: <Leadership />,
+      },
+      {
+        path: "/board",
+        element: <Board />,
+      },
+      {
+        path: "/our-services",
+        element: <Services />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
 export default App;
